2163d94b1a56ef43c2c1261b8d27998bb2086aca
Joe <joe@xxtime.com>
2024-02-04 23:04:22 +0800

    docs

--- deploy.sh
+++ deploy.sh
@@ -1,12 +1,18 @@
 #!/bin/bash
 # @docs https://docs.docker.com/engine/reference/commandline/container_run/
 
-image="zlabwork/php-dev:20240204-1707040335"
+DOCKER_IMAGE="zlabwork/php-dev:20240204-1707044243"
+DOCKER_NAME="dev"
 
-docker run --name zlabwork-service \
+docker run --name $DOCKER_NAME \
     -d \
     -it \
     -v ./:/app \
     -v /var/shared:/var/shared \
-    -p 8080:80 \
-    $image
+    -v ./docs/docker/conf.d:/etc/nginx/conf.d \
+    -p 8080:8080 \
+    $DOCKER_IMAGE
+
+# docker exec $DOCKER_NAME chroot --userspec=apache:apache / ls /
+docker exec $DOCKER_NAME service php8.1-fpm start
+docker exec $DOCKER_NAME service nginx start
